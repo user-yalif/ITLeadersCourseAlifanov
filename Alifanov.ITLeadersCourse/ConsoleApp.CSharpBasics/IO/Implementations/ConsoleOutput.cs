9d95839c7cdd6eff01b483cb3072c4aa1de3c613
Yury Alifanov <yury.alifanov@dataart.com>
2021-09-27 21:35:55 +0300

    Varibles and Operators lesson is over

---
+++ Alifanov.ITLeadersCourse/ConsoleApp.CSharpBasics/IO/Implementations/ConsoleOutput.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp.CSharpBasics.IO.Implementations
+{
+    using System;
+    using ConsoleApp.CSharpBasics.IO.Iterfaces;
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(object output = null)
+        {
+            Console.Write(output);
+        }
+
+        public void WriteLine(object output = null)
+        {
+            if (output == null)
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
