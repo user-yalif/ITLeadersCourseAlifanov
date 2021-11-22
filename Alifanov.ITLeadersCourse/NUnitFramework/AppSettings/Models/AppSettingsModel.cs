7b2baf0c6959aa3790e63291acf1fbb358072d6d
Yury Alifanov <yury.alifanov@dataart.com>
2021-11-22 21:33:41 +0200

    NUnit intro

---
+++ Alifanov.ITLeadersCourse/NUnitFramework/AppSettings/Models/AppSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NUnitFramework.AppSettings.Models
+{
+    using NUnitFramework.Enums;
+
+    public class AppSettingsModel
+    {
+        public string BaseUrl { get; set; }
+
+        public BrowserType Browser { get; set; }
+
+        public LoggerType Logger { get; set; }
+    }
+}
