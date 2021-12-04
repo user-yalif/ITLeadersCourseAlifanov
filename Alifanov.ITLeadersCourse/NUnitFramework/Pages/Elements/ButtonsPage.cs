65f65e9dcc90d9ab9128f8e6ad63eeecd6718b0e
Vera Kolubelova <vera.kolubelova@gmail.com>
2021-12-04 13:39:32 +0200

    IT-5 Double-Click-Me-Button

---
+++ Alifanov.ITLeadersCourse/NUnitFramework/Pages/Elements/ButtonsPage.cs
@@ -0,0 +1,34 @@
+﻿using NUnitFramework.Logging;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace NUnitFramework.Pages.Elements
+{
+    public class ButtonsPage : BasePage
+    {
+        private IWebElement DoubleClickMeButton => FindElement(By.CssSelector("button#doubleClickBtn"));
+
+        private IWebElement DoubleClickButtonMessage => FindElement(By.CssSelector("p#doubleClickMessage"));
+
+        public ButtonsPage ClickOnDoubleClickMeButton()
+        {
+            Logger.Log.Info("Click on Double Click Me button");
+
+            Actions actions = new Actions(Driver);
+
+            actions.DoubleClick(DoubleClickMeButton);
+            actions.Build()
+                .Perform();
+
+            return this;
+        }
+
+        public string GetMessageAfterDoubleClick()
+        {
+            Logger.Log.Info("Get message after clicking on Double Click Me button");
+
+            return DoubleClickButtonMessage.Text;
+
+        }
+    }
+}
