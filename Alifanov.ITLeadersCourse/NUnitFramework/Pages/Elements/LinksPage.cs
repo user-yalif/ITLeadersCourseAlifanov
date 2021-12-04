8202a167770449c63a2281401fa100cdec065a70
Vera Kolubelova <vera.kolubelova@gmail.com>
2021-12-04 18:17:16 +0200

    IT-19 Elements: Links: Created link

---
+++ Alifanov.ITLeadersCourse/NUnitFramework/Pages/Elements/LinksPage.cs
@@ -0,0 +1,27 @@
+﻿using NUnitFramework.Logging;
+using OpenQA.Selenium;
+
+namespace NUnitFramework.Pages.Elements
+{
+    public class LinksPage : BasePage
+    {
+        private IWebElement CreatedLink => FindElement(By.CssSelector("a#created"));
+
+        private IWebElement OutputCreatedLink => FindElement(By.Id("linkResponse"));
+
+
+        public LinksPage ClickOnCreatedLink()
+        {
+            Logger.Log.Info("Click on Created Link");
+
+            CreatedLink.Click();
+
+            return this;
+        }
+
+        public string GetOutput()
+        {
+            return OutputCreatedLink.Text;
+        }
+    }
+}
---
+++ Alifanov.ITLeadersCourse/NUnitFramework/Tests/Elements/LinksTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using NUnitFramework.Navigation.LeftPanel;
+
+namespace NUnitFramework.Tests.Elements
+{
+    class LinksTests : BaseTest
+    {
+        [Test]
+        [Description("Test clicks on Created Link and get output and then asserts that output is correct")]
+        [TestCase("Link has responded with staus 201 and status text Created")]
+
+        public void ClickOnCreatedLink(string expectedMessage)
+        {
+            var linksPage = LeftPanel.Elements.Links();
+
+            var actualMessage = linksPage.ClickOnCreatedLink()
+                .GetOutput();
+
+            Assert.That(actualMessage, Is.EqualTo(expectedMessage), "Messages are not equal");
+        }
+    }
+}
