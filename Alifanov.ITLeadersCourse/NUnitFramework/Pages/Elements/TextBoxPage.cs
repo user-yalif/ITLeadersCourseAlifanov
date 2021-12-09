44991f554d1703522f64b6b5fb0febb63a5c6edd
1SerhiiQA <1serhiiqa@gmail.com>
2021-12-09 18:56:52 +0200

    IT-4 updated, but test finished with status FAILED

--- Alifanov.ITLeadersCourse/NUnitFramework/Pages/Elements/TextBoxPage.cs
+++ Alifanov.ITLeadersCourse/NUnitFramework/Pages/Elements/TextBoxPage.cs
@@ -15,7 +15,11 @@ namespace NUnitFramework.Pages.Elements
 
         private IWebElement SubmitButton => FindElement(By.CssSelector("button#submit"));
 
-        private IWebElement NameInOutputUserForm => FindElement(By.CssSelector("p#name"));
+        //private IWebElement NameInOutputUserForm => FindElement(By.CssSelector("p#name"));
+
+        private IWebElement NameInOutputUserForm => FindElement(By.CssSelector("p#name.mb-1"));
+
+        //private IWebElement NameInOutputUserForm => FindElement(By.XPath("//div[@class='mb-1'][@id='name']"));
 
         private IWebElement EmailInOutputUserForm => FindElement(By.CssSelector("p#email"));
 
@@ -23,6 +27,36 @@ namespace NUnitFramework.Pages.Elements
 
         private IWebElement PermanentAddressInOutputUserForm => FindElement(By.CssSelector("p#permanentAddress"));
 
+        public TextBoxPage InputFullName(string expectedFullNameMessage)
+        {
+            FullNameField.Clear();
+            FullNameField.SendKeys(expectedFullNameMessage);
+
+            return this;
+        }
+
+        public TextBoxPage InputUserEmail(string expectedUserEmailMessage)
+        {
+            UserEmailField.Clear();
+            UserEmailField.SendKeys(expectedUserEmailMessage);
+
+            return this;
+        }
+        public TextBoxPage InputCurrentAddress(string expectedCurrentAddressMessage)
+        {
+            CurrentAddressField.Clear();
+            CurrentAddressField.SendKeys(expectedCurrentAddressMessage);
+
+            return this;
+        }
+
+        public TextBoxPage InputPermanentAddress(string expectedPermanentAddressMessage)
+        {
+            PermanentAddressField.Clear();
+            PermanentAddressField.SendKeys(expectedPermanentAddressMessage);
+
+            return this;
+        }
 
         public TextBoxPage ClickOnSubmitButton()
         {
@@ -37,28 +71,29 @@ namespace NUnitFramework.Pages.Elements
         {
             Logger.Log.Info("Get Name after click on Submit button");
 
-            return NameInOutputUserForm.Text;
+            return NameInOutputUserForm.Text.Split(":")[1];
+            //return NameInOutputUserForm.Text.Split(":")[1].TrimStart().TrimEnd();
         }
 
         public string GetEmailInUserForm()
         {
             Logger.Log.Info("Get Email after click on Submit button");
 
-            return EmailInOutputUserForm.Text;
+            return EmailInOutputUserForm.Text.Split(":")[1];
         }
 
         public string GetCurrentAddressInUserForm()
         {
             Logger.Log.Info("Get Current Address after click on Submit button");
 
-            return CurrentAddressInOutputUserForm.Text;
+            return CurrentAddressInOutputUserForm.Text.Split(":")[1];
         }
 
         public string GetPermanentAddressInUserForm()
         {
             Logger.Log.Info("Get Permanent Address after click on Submit button");
 
-            return PermanentAddressInOutputUserForm.Text;
+            return PermanentAddressInOutputUserForm.Text.Split(":")[1];
         }
     }
 }
--- Alifanov.ITLeadersCourse/NUnitFramework/Tests/Elements/TextBoxTests.cs
+++ Alifanov.ITLeadersCourse/NUnitFramework/Tests/Elements/TextBoxTests.cs
@@ -9,53 +9,53 @@ namespace NUnitFramework.Tests.Elements
         [Test]
         [Description("Test of Text Box (User Form) with clicks on Submit button and get messages and then asserts that messages are correct")]
         [TestCase("John Smith")]
-        public void CompareFullNameAfterCliclOnSubmitButton(string expectedMessage)
+        public void CompareFullNameAfterClickOnSubmitButton(string expectedFullNameMessage)
         {
-            var textBoxPage = LeftPanel.Elements.TextBox();
+            var textBoxPage = LeftPanel.Elements.TextBox();          
 
             var actualMessage = textBoxPage.ClickOnSubmitButton()
                 .GetNameInUserForm();
 
-            Assert.That(actualMessage, Is.EqualTo(expectedMessage), "Message are not equal");
+            Assert.That(actualMessage, Is.EqualTo(expectedFullNameMessage), "Message are not equal");
         }
 
         [Test]
         [Description("Test of Text Box (User Form) with clicks on Submit button and get messages and then asserts that messages are correct")]
         [TestCase("john@smith.com")]
-        public void CompareEmailAfterCliclOnSubmitButton(string expectedMessage)
+        public void CompareEmailAfterClickOnSubmitButton(string expectedUserEmailMessage)
         {
             var textBoxPage = LeftPanel.Elements.TextBox();
 
             var actualMessage = textBoxPage.ClickOnSubmitButton()
                 .GetEmailInUserForm();
 
-            Assert.That(actualMessage, Is.EqualTo(expectedMessage), "Message are not equal");
+            Assert.That(actualMessage, Is.EqualTo(expectedUserEmailMessage), "Message are not equal");
         }
 
         [Test]
         [Description("Test of Text Box (User Form) with clicks on Submit button and get messages and then asserts that messages are correct")]
         [TestCase("Downing street")]
-        public void CompareCurrentAddressAfterCliclOnSubmitButton(string expectedMessage)
+        public void CompareCurrentAddressAfterClickOnSubmitButton(string expectedCurrentAddressMessage)
         {
             var textBoxPage = LeftPanel.Elements.TextBox();
 
             var actualMessage = textBoxPage.ClickOnSubmitButton()
                 .GetCurrentAddressInUserForm();
 
-            Assert.That(actualMessage, Is.EqualTo(expectedMessage), "Message are not equal");
+            Assert.That(actualMessage, Is.EqualTo(expectedCurrentAddressMessage), "Message are not equal");
         }
 
         [Test]
         [Description("Test of Text Box (User Form) with clicks on Submit button and get messages and then asserts that messages are correct")]
         [TestCase("Buckingham Palace")]
-        public void ComparePermanentAddressAfterCliclOnSubmitButton(string expectedMessage)
+        public void ComparePermanentAddressAfterClickOnSubmitButton(string expectedPermanentAddressMessage)
         {
             var textBoxPage = LeftPanel.Elements.TextBox();
 
             var actualMessage = textBoxPage.ClickOnSubmitButton()
                 .GetPermanentAddressInUserForm();
 
-            Assert.That(actualMessage, Is.EqualTo(expectedMessage), "Message are not equal");
+            Assert.That(actualMessage, Is.EqualTo(expectedPermanentAddressMessage), "Message are not equal");
         }
     }
 }
