bbda195fd3753c269a5ee7c6bc04ef3ee0622254
Vera Kolubelova <vera.kolubelova@gmail.com>
2021-12-21 13:06:44 +0200

    IT-33-Widgets-Tabs Check tabs to be clickable, get and verify text of tabs

--- Alifanov.ITLeadersCourse/NUnitFramework/Navigation/LeftPanel/LeftPanel.cs
+++ Alifanov.ITLeadersCourse/NUnitFramework/Navigation/LeftPanel/LeftPanel.cs
@@ -2,11 +2,14 @@
 {
     using NUnitFramework.Navigation.LeftPanel.Groups.AlertsFrameAndWindows;
     using NUnitFramework.Navigation.LeftPanel.Groups.Elements;
+    using NUnitFramework.Navigation.LeftPanel.Groups.Widgets;
 
     public static class LeftPanel
     {
         public static ElementsGroup Elements => new();
 
         public static AlertsFrameAndWindowsGroup AlertsFrameAndWindows => new();
+
+        public static WidgetsGroup Widgets => new();
     }
 }
---
+++ Alifanov.ITLeadersCourse/NUnitFramework/Tests/Widgets/TabsTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using NUnitFramework.Navigation.LeftPanel;
+
+namespace NUnitFramework.Tests.Widgets
+{
+    public class TabsTests : BaseTest
+    {
+        [Test]
+        [Description("Test checks tabs to be clickable")]
+        public void AssertThatTabsAreClickableTest()
+        {
+            var tabsPage = LeftPanel.Widgets.Tabs();
+
+            tabsPage.ClickOnTab("origin");
+            tabsPage.ClickOnTab("use");
+            tabsPage.ClickOnTab("more");
+            tabsPage.ClickOnTab("what");
+        }
+
+        [Test]
+        [Description("Test clicks ob Tab, gets Tab's text and checks if text content the given one")]
+        public void AssertThatTextOfTabsIsCorect()
+        {
+            var tabsPage = LeftPanel.Widgets.Tabs();
+
+            tabsPage.ClickOnTab("origin");
+
+            string actualOriginTabText = tabsPage.GetOriginTabText();
+            string expectedOriginTabText = "de Finibus Bonorum et Malorum";
+            Assert.That(actualOriginTabText.Contains(expectedOriginTabText), "Expected text doesn't exist");
+
+            tabsPage.ClickOnTab("use");
+
+            string actualUseTabText = tabsPage.GetUSeTabText();
+            string expectedUseTabText = "'Content here, content here'";
+            Assert.That(actualUseTabText.Contains(expectedUseTabText), "Expected text doesn't exist");
+
+            tabsPage.ClickOnTab("what");
+
+            string actualWhatTabText = tabsPage.GetWhatTabText();
+            string expectedWhatTabText = "Lorem Ipsum is simply dummy text";
+            Assert.That(actualWhatTabText.Contains(expectedWhatTabText), "Expected text doesn't exist");
+        }
+    }
+}
