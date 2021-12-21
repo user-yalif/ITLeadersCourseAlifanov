d4bb776d3a9a202002831cd9cbbe6b2b43f3b61d
1SerhiiQA <1serhiiqa@gmail.com>
2021-12-21 20:49:50 +0200

    PracticeFormPage.cs and ThanksForSubmittingForm.cs are created

---
+++ Alifanov.ITLeadersCourse/NUnitFramework/Pages/Forms/PracticeFormElements/ThanksForSubmittingForm.cs
@@ -0,0 +1,84 @@
+﻿using NUnitFramework.Pages.Forms.PracticeForms;
+using OpenQA.Selenium;
+
+namespace NUnitFramework.Pages.Forms.PracticeFormElements
+{
+    class ThanksForSubmittingForm
+    {
+        private IWebElement ThanksForSubmittingFormElements { get; }
+
+        private IWebElement Title => ThanksForSubmittingFormElements.FindElement(By.CssSelector("div#example-modal-sizes-title-lg"));
+
+        private IWebElement StudentNameValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[1]/td[2]"));
+
+        private IWebElement StudentEmailValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[2]/td[2]"));
+
+        private IWebElement GenderRadioValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[3]/td[2]"));
+
+        private IWebElement MobileValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[4]/td[2]"));
+
+        private IWebElement DateOfBirthValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[5]/td[2]"));
+
+        private IWebElement SubjectsValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[6]/td[2]"));
+
+        private IWebElement HobbiesCheckBoxValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[7]/td[2]"));
+
+        private IWebElement UploadPicturePreview => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[8]/td[2]"));
+
+        private IWebElement AddressValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[9]/td[2]"));
+
+        private IWebElement StateAndCityValue => ThanksForSubmittingFormElements.FindElement(By.XPath("//tr[10]/td[2]"));
+
+        private IWebElement CloseButton => ThanksForSubmittingFormElements.FindElement(By.CssSelector("button#closeLargeModal"));
+
+        public string GetStudentName()
+        {
+            return StudentNameValue.GetAttribute("value");
+        }
+
+        public string GetStudentEmail()
+        {
+            return StudentEmailValue.GetAttribute("value");
+        }
+        public string GetStudentGender()
+        {
+            return GenderRadioValue.GetAttribute("value");
+        }
+
+        public string GetStudentMobile()
+        {
+            return MobileValue.GetAttribute("value");
+        }
+
+        public string GetStudentDateOfBirth()
+        {
+            return DateOfBirthValue.GetAttribute("value");
+        }
+
+        public string GetStudentSubjects()
+        {
+            return SubjectsValue.GetAttribute("value");
+        }
+
+        public string GetStudentHobbies()
+        {
+            return HobbiesCheckBoxValue.GetAttribute("value");
+        }
+
+        public string GetStudentAddress()
+        {
+            return AddressValue.GetAttribute("value");
+        }
+
+        public string GetStudentStateAndCity()
+        {
+            return StateAndCityValue.GetAttribute("value");
+        }
+        public ThanksForSubmittingForm ClickCloseButton()
+        {
+            CloseButton.Click();
+
+            return new PracticeFormPage();
+        }
+    }
+}
--- Alifanov.ITLeadersCourse/NUnitFramework/Pages/Forms/PracticeFormPage.cs
+++ Alifanov.ITLeadersCourse/NUnitFramework/Pages/Forms/PracticeFormPage.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
 
 namespace NUnitFramework.Pages.Forms.PracticeForms
 {
@@ -10,11 +12,7 @@ namespace NUnitFramework.Pages.Forms.PracticeForms
 
         private IWebElement EmailInput => FindElement(By.CssSelector("input#userEmail"));
 
-        private IWebElement GenderRadioMale => FindElement(By.CssSelector("input#gender-radio-1"));
-
-        private IWebElement GenderRadioFemale => FindElement(By.CssSelector("input#gender-radio-2"));
-
-        private IWebElement GenderRadioOther => FindElement(By.CssSelector("input#gender-radio-3"));
+        private IList<IWebElement> RadioButtons => FindElements(By.XPath("//div[contains(@class, 'custom-radio')]"));
 
         private IWebElement MobileInput => FindElement(By.CssSelector("input#userNumber"));
 
@@ -22,20 +20,143 @@ namespace NUnitFramework.Pages.Forms.PracticeForms
 
         private IWebElement SubjectsInput => FindElement(By.CssSelector("input#subjectsInput"));
 
-        private IWebElement HobbiesWrapperSports => FindElement(By.CssSelector("input#hobbies-checkbox-1"));
-
-        private IWebElement HobbiesWrapperReading => FindElement(By.CssSelector("input#hobbies-checkbox-2"));
-
-        private IWebElement HobbiesWrapperMusic => FindElement(By.CssSelector("input#hobbies-checkbox-3"));
+        private IList<IWebElement> CheckBoxes => FindElements(By.XPath("//div[contains(@class, 'custom-checkbox')]"));
 
         private IWebElement PictureUploadButton => FindElement(By.CssSelector("input#uploadPicture"));
 
         private IWebElement CurrentAddressInput => FindElement(By.CssSelector("textarea#currentAddress"));
 
-        private IWebElement SelectStateContainer => FindElement(By.CssSelector("div#state.css-tlfecz-indicatorContainer"));
-
-        private IWebElement SelectCityContainer => FindElement(By.CssSelector("div#city.css-tlfecz-indicatorContainer"));
+        private IWebElement StateSelect => FindElement(By.XPath("//div[@id='state']//input"));
+        private IList<IWebElement> StateSelectOptions => FindElements(By.XPath("//div[contains(@id, 'react-select-3-option')]"));
+        private IWebElement CitySelect => FindElement(By.XPath("//div[@id='city']//input"));
+        private IList<IWebElement> CitySelectOptions => FindElements(By.CssSelector("//div[contains(@id, 'react-select-4-option')]"));
 
         private IWebElement SubmitButton => FindElement(By.CssSelector("button#submit"));
+
+        public PracticeFormPage InputFirstName(string firstName)
+        {
+            FirstNameInput.SendKeys(firstName);
+
+            return this;
+        }
+
+        public PracticeFormPage InputLastName(string lastName)
+        {
+            LastNameInput.SendKeys(lastName);
+
+            return this;
+        }
+
+        public PracticeFormPage InputEmail(string email)
+        {
+            EmailInput.SendKeys(email);
+
+            return this;
+        }
+
+        public PracticeFormPage ClickOnRadioButton(string gender)
+        {
+            foreach (var radioButton in RadioButtons)
+            {
+                var name = radioButton.FindElement(By.XPath("./label")).Text.Trim();
+
+                if (name.Equals(gender, StringComparison.OrdinalIgnoreCase))
+                {
+                    radioButton.Click();
+                }
+            }
+
+            return this;
+        }
+
+        public PracticeFormPage InputMobile(string mobile)
+        {
+            MobileInput.SendKeys(mobile);
+
+            return this;
+        }
+
+        public PracticeFormPage InputDateOfBirth(string dateOfBirth)
+        {
+            DateOfBirthInput.SendKeys(dateOfBirth);
+
+            return this;
+        }
+
+        public PracticeFormPage ClickOnCheckBox(string hobby)
+        {
+            foreach (var checkBox in CheckBoxes)
+            {
+                var name = checkBox.FindElement(By.XPath("./label")).Text.Trim();
+
+                if (name.Equals(hobby, StringComparison.OrdinalIgnoreCase))
+                {
+                    checkBox.Click();
+                }
+            }
+
+            return this;
+        }
+        public PracticeFormPage InputCurrentAddress(string currentAddress)
+        {
+            CurrentAddressInput.SendKeys(currentAddress);
+
+            return this;
+        }
+        public void SelectStateOption(string state)
+        {
+            string option = null;
+
+            foreach (var stateOption in StateSelectOptions)
+            {
+                var optionName = stateOption.Text;
+
+                if (state.Equals(optionName, StringComparison.OrdinalIgnoreCase))
+                {
+                    option = optionName;
+                }
+            }
+
+            StateSelect.SendKeys(option);
+        }
+
+        public void SelectStateOptions(string city)
+        {
+            foreach (var element in CitySelectOptions)
+            {
+                if (city.Equals(element.Text, StringComparison.OrdinalIgnoreCase))
+                {
+                    element.Click();
+                }
+            }
+        }
+
+        public void SelectCityOption(string city)
+        {
+            string option = null;
+
+            foreach (var cityOption in CitySelectOptions)
+            {
+                var optionName = cityOption.Text;
+
+                if (city.Equals(optionName, StringComparison.OrdinalIgnoreCase))
+                {
+                    option = optionName;
+                }
+            }
+
+            CitySelect.SendKeys(option);
+        }
+
+        public void SelectCityOptions(string city)
+        {
+            foreach (var element in CitySelectOptions)
+            {
+                if (city.Equals(element.Text, StringComparison.OrdinalIgnoreCase))
+                {
+                    element.Click();
+                }
+            }
+        }
     }
 }
